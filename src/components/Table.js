994fa82fffc6e8feeff63ab6d7697465956483f4
The Farrier <nkfarrey@gmail.com>
2020-06-08 18:29:15 -0500

    created filter function for dropdown component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,7 @@
 import React from "react";
 import List from "./List";
 import employees from "./employees.json"
+import DropdownList from "./DropdownList";
 
 class Table extends React.Component{
   state = {
@@ -13,9 +14,17 @@ class Table extends React.Component{
     })
   };
 
-  filter = (department => {
-
-  })
+  filterby = (dept) => {
+    if(dept === "All"){
+      this.setState({
+        employees: employees
+      })
+    } else {
+      this.setState({
+        employees: employees.slice().filter(e=>e.department === dept)
+      })
+    }
+  };
 
 
   render() {
@@ -23,10 +32,10 @@ class Table extends React.Component{
       <table className="table">
         <thead>
           <tr>
-            <th scope="col" ><button onClick={() => this.sortby("id")}>ID</button></th>
-            <th scope="col" ><button onClick={() => this.sortby("name")}>Name</button></th>
-            <th scope="col" ><button onClick={() => this.sortby("position")}>Position</button></th>
-            <th scope="col" ><button onClick={() => this.sortby("department")}>Department</button ></th>
+            <th scope="col" ><button class="btn btn-secondary btn-md" onClick={() => this.sortby("id")}>ID</button></th>
+            <th scope="col" ><button class="btn btn-secondary btn-md" onClick={() => this.sortby("name")}>Name</button></th>
+            <th scope="col" ><button class="btn btn-secondary btn-md" onClick={() => this.sortby("position")}>Position</button></th>
+            <th scope="col" ><DropdownList filterby={this.filterby} sort={this.sortby}/></th>
           </tr>
         </thead>
         <List employees={this.state.employees}/>
