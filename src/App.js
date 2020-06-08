2e2c63bc56dc78bca2ee7f5c1bfbb5b0129355d6
The Farrier <nkfarrey@gmail.com>
2020-06-07 19:33:58 -0500

    corrected router and wrapper imports

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Wrapper from '../../../../UTSA-SAN-FSF-PT-01-2020-U-C/19-React/01-Activities/23-Stu_PupsterApp/Solved_/src/components/Wrapper';
+import Wrapper from "./components/Wrapper";
+import Table from "./components/Table"
 
 
 function App() {
   return (
-    <Router>
-      <Wrapper>
-        <Table />
-      </Wrapper>
-    </Router>
+    <Wrapper>
+      <Table />
+    </Wrapper>
   );
 }
 
