c62a97e60e814050f919d8c481755529f7207e13
The Farrier <nkfarrey@gmail.com>
2020-06-07 19:33:22 -0500

    created sorting functionality for table

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,20 +2,37 @@ import React from "react";
 import List from "./List";
 import employees from "./employees.json"
 
-function Table(props) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col">Position</th>
-          <th scope="col">Department</th>
-        </tr>
-      </thead>
-      <List employees={employees}/>
-    </table>
-  );
+class Table extends React.Component{
+  state = {
+    employees: employees
+  };
+
+  sortby = (column) => {
+    this.setState({
+      employees: this.state.employees.sort((a,b)=>(a[column]>b[column]) ? 1 :-1)
+    })
+  };
+
+  filter = (department => {
+
+  })
+
+
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col" ><button onClick={() => this.sortby("id")}>ID</button></th>
+            <th scope="col" ><button onClick={() => this.sortby("name")}>Name</button></th>
+            <th scope="col" ><button onClick={() => this.sortby("position")}>Position</button></th>
+            <th scope="col" ><button onClick={() => this.sortby("department")}>Department</button ></th>
+          </tr>
+        </thead>
+        <List employees={this.state.employees}/>
+      </table>
+    );
+  }
 }
 
 export default Table;
